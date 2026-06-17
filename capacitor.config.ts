b4eb199e1df5c9e395e8c6d350c8b845ed835859
MohamedIbrahim71 <mohamed@example.com>
2026-06-17 18:02:49 +0300

    Change capacitor config to JSON

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ieltsplanner.app',
-  appName: 'IELTS Planner',
-  webDir: 'www'
-};
-
-export default config;
